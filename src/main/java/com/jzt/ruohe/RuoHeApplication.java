376851a996d695a1a78eb0520137fb1529cc43e2
bluejww <bluejww@163.com>
2019-07-23 20:49:29 +0800

    RuoHe project

---
+++ src/main/java/com/jzt/ruohe/RuoHeApplication.java
@@ -0,0 +1,13 @@
+package com.jzt.ruohe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuoHeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuoHeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jzt/ruohe/RuoHeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jzt.ruohe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RuoHeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
