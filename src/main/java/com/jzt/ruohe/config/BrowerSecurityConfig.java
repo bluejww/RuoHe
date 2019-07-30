bc670f2a6f3669e83ff1d0f39f12bd586b011a62
bluejww <bluejww@163.com>
2019-07-30 17:40:08 +0800

    用户权限处理

--- src/main/java/com/jzt/ruohe/config/BrowerSecurityConfig.java
+++ src/main/java/com/jzt/ruohe/config/BrowerSecurityConfig.java
@@ -1,7 +1,32 @@
 package com.jzt.ruohe.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class BrowerSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO 
+		http.formLogin() //  定义当需要用户登录时候，转到的登录页面
+		.loginPage("/templates/login.html")
+		.loginProcessingUrl("/user/login")
+		.and()
+		.authorizeRequests() // 定义哪些URL需要被保护、哪些不需要被保护
+		.antMatchers("/blogserver/src/main/resources/templates/login.html")
+		.permitAll()
+		.anyRequest()  // 任何请求,登录后可以访问
+		.authenticated()
+		.and()
+		.csrf().disable();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	     return new BCryptPasswordEncoder();
+	 }
 
 }
--- src/main/java/com/jzt/ruohe/config/JztUserDetailsService.java
+++ src/main/java/com/jzt/ruohe/config/JztUserDetailsService.java
@@ -2,20 +2,36 @@ package com.jzt.ruohe.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * 配置用户认证逻辑，因为我们是有自己的一套用户体系的
+ * 
+ * @author jianww
+ *
+ */
 public class JztUserDetailsService implements UserDetailsService {
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	
+	@Autowired
+    private PasswordEncoder passwordEncoder;
+
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		logger.info("user name is : "+username);
-		return null;
+		logger.info("user name is : " + username);
+		
+		String passward = passwordEncoder.encode("123456");
+		
+		// 根据用户名查找对应的密码权限. 参数分别是：用户名，密码，用户权限
+		User user = new User(username, passward, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+		return user;
 	}
-	
-	
 
 }
